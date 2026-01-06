6d96b40622ec5f444ea041c3da2fb2a4ac137f97
Toni <hidden.toni@pm.me>
2026-01-06 22:50:20 +0100

    game-lib available for bot-programming

--- bot-programming/ultimate-tic-tac-toe/src/main.rs
+++ bot-programming/ultimate-tic-tac-toe/src/main.rs
@@ -6,8 +6,8 @@ fn main() {
         let ttt = TicTacToe::new();
         let (opp, moves) = crate::parse::read_input();
 
+        println!("0 0");
         // Write an action using println!("message...");
         // To debug: eprintln!("Debug message...");
-        println!("0 0");
     }
 }
